a16e21aa4d7993ce54fbe8bb5ec0b344de236d78
mediadnan <mediadnan@gmail.com>
2022-06-23 14:20:08 +0100

    added __version__ to __all__

--- funchain/__init__.py
+++ funchain/__init__.py
@@ -2,7 +2,6 @@ from funchain.wrapper import chainable, funfact
 from funchain.chain import Chain, parse
 
 
-__all__ = ['chainable', 'funfact', 'Chain', 'parse']
 __version__ = '0.0.1'
 __doc__ = """funchain package provides tools and classes to create ready to use chains, 
 the main goal is to ease chaining functions and capture errors without breaking the whole program.
@@ -12,3 +11,10 @@ the main user interface tools are Chain class, parse function and the two decora
 Chain is the main component, it parses the given model and creates the right execution structure.
 it also takes a callback that takes a report as its only parameter.
 chains are callables, they take an input and returns the last result."""
+__all__ = [
+    chainable,
+    funfact,
+    Chain,
+    parse,
+    __version__
+]
