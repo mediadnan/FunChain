62412497deb805bc054e1011fb362b28c9b75384
mediadnan <mediadnan@gmail.com>
2022-06-23 17:25:28 +0100

    added test for project, testing version synchronization

---
+++ tests/test_meta.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from configparser import ConfigParser
+from funchain import __version__
+
+
+root = Path(__file__).parent.parent
+
+
+def test_sync_version():
+    """ensures the version is updated anywhere"""
+    config = ConfigParser()
+    config.read(root / 'setup.cfg')
+    assert __version__ == config['metadata']['version'], "version should be updated everywhere"
