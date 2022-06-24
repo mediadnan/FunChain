ceffc3f998a2d3dd6e778d7231259c4f33627793
mediadnan <mediadnan@gmail.com>
2022-06-24 09:05:22 +0100

    moved all setup config to setup.cfg

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
-from setuptools import setup, find_packages
-import pathlib
-from funchain import __version__
-
-
-current = pathlib.Path(__file__).parent.resolve()
-long_description = (current / "README.md").read_text(encoding="utf-8")
+from setuptools import setup
 
 
 if __name__ == '__main__':
-    setup(
-        name="funchain",
-        version=__version__,
-        license="MIT",
-
-        description="tool for chain functions easily and safely",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-
-        url="https://github.com/mediadnan/funchain",
-        project_urls={
-            "bug-report": "https://github.com/mediadnan/funchain/issues"
-        },
-
-        author="mediadnan",
-        author_email="mediadnan@gmail.com",
-
-    )
+    setup()
