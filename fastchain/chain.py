b25fbd717b89687b7e4635dfc8070b51717a8cc7
mediadnan <mediadnan@gmail.com>
2022-07-24 08:34:14 +0100

    added

--- fastchain/chain.py
+++ fastchain/chain.py
@@ -1,12 +1,14 @@
-import re
-from typing import Callable, Pattern
+from typing import Callable, overload, TypeVar, Any
+
+from ._tools import validate_name
 from .factory import parse
 from .chainables import CHAINABLE_OBJECTS
 from .monitoring import ReportDetails, Report, create_report_maker
 
-CHAIN_NAME: Pattern[str] = re.compile(r'^\w[\w\d_-]+?$', re.DOTALL)
 LOG_FAILURES: bool = True
 RAIS_FOR_FAIL: bool = False
+
+T = TypeVar('T')
 
 
 class Chain:
@@ -20,16 +22,14 @@ class Chain:
             raise_for_fail: bool = RAIS_FOR_FAIL
     ):
         """
-        creates a chain and globally registers it.
+        creates a new chain with the given structure.
 
         :param name: the chains name must be unique.
         :param chainables: any supported 'chainable' object.
         :key log_failures: whether to log failures or not, default to True.
+        :key raise_for_fail: whether to raise exception for fatal failures (default None).
         """
-        if not isinstance(name, str):
-            raise TypeError("the name of the chain must be a string.")
-        elif not CHAIN_NAME.match(name):
-            raise ValueError("chain's name must begin with a letter and only contain letter, digits, _ and -")
+        validate_name(name)
         core = parse(chainables)
 
         self.__name: str = name
@@ -49,7 +49,7 @@ class Chain:
     def __repr__(self) -> str:
         return f'<chain {self.__name!r}>'
 
-    def __call__(self, input, reports: dict[str, ReportDetails] | None = None):
+    def __call__(self, input: Any, reports: dict[str, ReportDetails] | None = None) -> Any:
         """
         processes the given input and returns the result,
         the chain creates its report and only registers it
@@ -64,3 +64,63 @@ class Chain:
         if reports is not None:
             reports[self.__name] = report.make()
         return result
+
+
+class ChainMaker:
+    """
+    Utility object for making a group of chains with the same configuration and same prefix.
+    """
+    __slots__ = 'name', 'log_failures', 'raise_for_fail', '__registered_chains__'
+
+    def __init__(self, name: str, *, log_failures: bool = LOG_FAILURES, raise_for_fail: bool = RAIS_FOR_FAIL):
+        self.name: str = validate_name(name)
+        self.log_failures: bool = log_failures
+        self.raise_for_fail: bool = raise_for_fail
+        self.__registered_chains__: dict[str, Chain] = {}
+
+    @staticmethod
+    def _case_insensitive_name(name: str) -> str:
+        if not isinstance(name, str):
+            raise TypeError("a name should be str")
+        return name.lower()
+
+    def __getitem__(self, name: str) -> Chain:
+        try:
+            return self.__registered_chains__[self._case_insensitive_name(name)]
+        except KeyError:
+            raise KeyError(f"no chain is registered with the name {name!r}")
+
+    def __contains__(self, name: str) -> bool:
+        return self._case_insensitive_name(name) in self.__registered_chains__
+
+    @overload
+    def get(self, name: str) -> Chain | None: ...
+    @overload
+    def get(self, name: str, default: T) -> Chain | T: ...
+
+    def get(self, name, default=None):
+        """
+        gets the registered chain by name, or returns default if it does not exist.
+
+        :param name: the name of the registered chain (case-insensitive).
+        :param default: the value to return if the chain does not exist, default None.
+        :return: the chain if it exists or default.
+        """
+        return self.__registered_chains__.get(self._case_insensitive_name(name), default)
+
+    def __call__(self, name: str, *chainables: CHAINABLE_OBJECTS) -> Chain:
+        """
+        creates a new chain with the same configuration and same prefix.
+
+        :param name: the name of the new chain.
+        :param chainables: the body of the new chain.
+        :return: the new created chain.
+        """
+        if name in self:
+            raise ValueError("a chain with the same name already been registered.")
+        return Chain(
+            f"{self.name}_{validate_name(name)}",
+            *chainables,
+            log_failures=self.log_failures,
+            raise_for_fail=self.raise_for_fail
+        )
