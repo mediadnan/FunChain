2691b91ced4db58aaa0dc5a964bdf3c4efefebcb
mediadnan <mediadnan@gmail.com>
2022-08-11 12:21:05 +0100

    monitoring.py fully tested

--- fastchain/monitoring.py
+++ fastchain/monitoring.py
@@ -1,18 +1,18 @@
 """
-This module holds implementations of all the monitoring tools that collect
-data processing statistics from chainables.
+This module implements Reporter and ReporterMaker and all the tools to chain's
+process monitoring and handling failures.
+
+It also provides a standard FailureHandler base class to be inherited and customized
+handling failures.
 """
 import abc
+import operator
 import warnings
 import logging
 import typing as tp
 
 from fastchain._abc import ReporterBase
-
-if tp.TYPE_CHECKING:
-    from fastchain.chainables import Chainable
-else:
-    Chainable = tp.Any
+from fastchain.chainables import Chainable, Node, Collection
 
 
 class FailureDetails(tp.TypedDict):
@@ -23,26 +23,52 @@ class FailureDetails(tp.TypedDict):
     fatal: bool
 
 
-class ReportDetails(tp.TypedDict):
-    """standard report details dictionary"""
-    rate: float
-    expected_rate: float
-    succeeded: int
-    missed: int
-    failed: int
-    total: int
-    failures: list[FailureDetails]
+class ReportStatistics(tp.TypedDict):
+    """standard reporter statistics dictionary"""
+    success_rate: float
+    minimum_expected_rate: float
+    successful_node_operations: int
+    failed_node_operations: int
+    missed_nodes: int
+    total_nodes: int
 
 
 class FailureHandler(abc.ABC):
+    """
+    Base class for failure handlers that will be called
+    when failures occur.
+
+    all subclasses must have the same __init__ signature
+    and must define the class name attribute
+    (ending with 'handler' for consistency)
+    and implement the __call__ method
+
+    Example:
+        >>> class SpecialHandler(FailureHandler):
+        ...     name: str = 'special handler'
+        ...     def __call__(self, failure: FailureDetails) -> None:
+        ...         ... # here goes the failure handling
+    """
     __slots__ = 'owner',
 
     def __init__(self, owner: str) -> None:
-        if owner is None:
-            raise ValueError(f"cannot create a {self.name} without owners name")
+        """
+        initializes the new failure handler.
+
+        :param owner: name of the chain owning this handler
+        :type owner: str
+        """
         self.owner: str = owner
 
     def message(self, failure: FailureDetails) -> str:
+        """
+        produces common failure string format
+
+        :param failure: failure obj / dict
+        :type failure: FailureDetails
+        :return: failure message
+        :rtype: str
+        """
         source, input, error = failure['source'], failure['input'], failure['error']
         return f"{self.owner}::{source} raised {error!r} after receiving {input!r} (type: {type(input).__qualname__})"
 
@@ -54,58 +80,58 @@ class FailureHandler(abc.ABC):
 
 
 class LoggingHandler(FailureHandler):
+    """handler logs failure with the standard logging.Logger with the owners name"""
     name: str = "logging handler"
 
     def __init__(self, owner: str) -> None:
         super().__init__(owner)
-        fmt = logging.Formatter("%(levelname)s %(message)s at %(asctime)s ", "%Y-%m-%d %H:%M:%S", '%')
-        sh = logging.StreamHandler()
-        sh.setFormatter(fmt)
-        logger = logging.getLogger(owner)
-        logger.addHandler(sh)
-        self.logger: logging.Logger = logger
+        self.logger: logging.Logger = logging.getLogger(owner)
 
     def __call__(self, failure: FailureDetails) -> None:
+        """logs the failure with a different level for different failure's severity"""
         lvl = logging.ERROR if failure['fatal'] else logging.INFO
         self.logger.log(lvl, self.message(failure), stacklevel=2)
 
 
 class Reporter(ReporterBase):
     """
-    Reporter objects get created internally by the chain when it gets called
-    to keep track of the execution information and stats between node calls,
-    register failures' exceptions and call registered handlers on them, and also
-    make a statistics report at the end.
+    Reporter object is used by the chain's internal component to report processing
+    success state and errors, it is responsible for recording failures
+    and produce statistics about the overall process.
 
-    Users are not supposed to create Reporter object this should be handled by
-    the chain and utilities that the chain uses unless the intentions are testing.
+    The reporter should never be created by users unless for testing purposes,
+    it does no validation (intentionally) to be more optimized as it will be
+    created every time the chain is called. And most of the preparation
+    are taken care by the ReporterMaker.
     """
-    __slots__ = 'counter', 'required', 'failures', 'failure_handlers',
+    __slots__ = 'counter', 'minimum_rate', 'failures', 'failure_handlers',
 
     def __init__(
             self,
-            components: frozenset[Chainable],
-            required: int,
+            components: frozenset[Node],
+            minimum_rate: float,
             failure_handlers: list[FailureHandler]
     ) -> None:
-        self.counter: dict[Chainable, list[bool]] = {component: [] for component in components}
-        self.required: int = required
+        self.counter: dict[Node, list[bool]] = {component: [] for component in components}
+        self.minimum_rate: float = minimum_rate
         self.failures: list[FailureDetails] = []
         self.failure_handlers: list[FailureHandler] = failure_handlers
 
-    def __call__(self, component: Chainable, success: bool) -> None:
+    def __call__(self, node: Node, success: bool) -> None:
         """
-        marks the registered component result as success or failure for statistics.
+        marks success state of a previously registered node
 
-        :param component: one of the previously registered counter.
-        :param success: either True for successful operations or False for unsuccessful ones.
+        :param node: registered node
+        :type node: Node
+        :param success: True if operation succeeded or False otherwise
+        :type success: bool
         :return: None
         """
         try:
-            self.counter[component].append(success)
+            self.counter[node].append(success)
         except KeyError:
             warnings.warn(
-                f"unregistered item {component} ignored",
+                f"unregistered node {node!r} ignored",
                 UserWarning,
                 stacklevel=2,
                 source=self
@@ -121,40 +147,41 @@ class Reporter(ReporterBase):
         """
         registers the failure to be reported.
 
-        :param source: the title of the reporter object.
+        :param source: the coll_title of the reporter object.
         :param input: the value that caused the failure.
         :param error: the risen exception.
-        :param fatal: True if the error is from a required component
+        :param fatal: True if the error is from a required node
         """
         failure = dict(source=source, input=input, error=error, fatal=fatal)
         self.failures.append(failure)
         for handler in self.failure_handlers:
             handler(failure)
 
-    def report(self) -> ReportDetails:
+    def statistics(self) -> ReportStatistics:
         """
-        builds a report dictionary with the following information;
+        builds a reporter statistics dictionary with the following information;
 
-        **rate** *(float)*
-            number between 0.0 - 1.0 ratio of registered success over total.
+        **success_rate** *(float)*
+            ratio (between 0.0 - 1.0) of registered node successes over total number of nodes
 
-        **expected_rate** *(float)*
-            number between 0.0 - 1.0 ratio of registered success over total required.
+        **minimum_expected_rate** *(float)*
+            ratio (between 0.0 - 1.0) of required nodes over total number of nodes
 
-        **succeeded** *(int)*
-            number of reported successful operations.
+        **successful_node_operations** *(int)*
+            number of reported successful operations from the same or different nodes
 
-        **missed** *(int)*
-            number of expected but not reported operations.
+        **failed_node_operations** *(int)*
+            number of reported failing operations from the same or different nodes
 
-        **failed** *(int)*
-            number of reported failing operations.
+        **missed_nodes** *(int)*
+            number of unreached chain nodes
 
-        **total** *(int)*
-            number of counter expected to succeed.
+        **total_nodes** *(int)*
+            total number of the chain's node
 
-        **failures** *(list[dict])*
-            a list of registered failure (source, input, error).
+        :return: dictionary of success_rate, minimum_expected_rate, successful_node_operations,
+                 failed_node_operations, missed_nodes and total_nodes
+        :rtype: dict
         """
         completed = 0.0
         successes = 0
@@ -172,40 +199,77 @@ class Reporter(ReporterBase):
             failures += failure_count
             completed += success_count / record_count
         return dict(
-            rate=round(completed / total, 4),
-            expected_rate=round(self.required / total, 4),
-            succeeded=successes,
-            missed=misses,
-            failed=failures,
-            total=total,
-            failures=self.failures
+            success_rate=round(completed / total, 4),
+            minimum_expected_rate=self.minimum_rate,
+            successful_node_operations=successes,
+            failed_node_operations=failures,
+            missed_nodes=misses,
+            total_nodes=total,
         )
 
 
 class ReporterMaker:
     """
-    ReporterMaker object is a reporter factory that prepares its initial information
-    to optimize its initialization when the chain get called.
-    """
-    __slots__ = 'components', 'required', 'failure_handlers'
+    ReporterMaker object is a reporter factory used by the chain to prepare
+    some initial state related to the chain itself and not to its processing input
+    to minimize the impact of creating reports for each call.
 
-    def __init__(self, name: str, components: tp.Iterable[Chainable], handlers: list[tp.Type[FailureHandler]]) -> None:
+    This layer has been added to separate thing that should be executed once (when
+    defining the chain) and things that will be executed each time the chain is triggered.
+
+    ReporterMaker is not expected to be created by users (unless for testing),
+    the full process is abstracted away and should be taken care of by the chain's constructor.
+    """
+    __slots__ = 'components', 'minimum_rate', 'failure_handlers',
+
+    def __init__(
+            self,
+            name: str,
+            chainable: Chainable,
+            handlers: list[tp.Type[FailureHandler]]
+    ) -> None:
         """
-        Initializes a new ReporterMaker object with the given information.
+        Initializes new ReporterMaker object with the given information.
 
         :param name: the name of the owner.
         :type name: str
-        :param components: chainables that will be reporting.
-        :type components: Iterable[Chainable]
+        :param chainable: chainables that will be reporting.
+        :type chainable: Iterable[Chainable]
         :param handlers: a list of FailureHandler subclasses to handle failures.
         :type handlers: list[FailureHandler]
         """
-        if not components:
-            raise ValueError("unable create reports without counter")
-        self.components: frozenset[Chainable] = frozenset(components)
-        self.required: int = [not getattr(component, 'optional', False) for component in self.components].count(True)
+        if not isinstance(chainable, Chainable):
+            raise TypeError("chainable must be an instance of Chainable subclass")
+        elif not all((isinstance(handler, type) and issubclass(handler, FailureHandler)) for handler in handlers):
+            raise TypeError("handlers must be a list of fastchain.monitoring.FailureHandler subclasses")
+        nodes = self.get_nodes(chainable)
+        self.components: frozenset[Node] = frozenset(nodes)
+        self.minimum_rate: float = operator.countOf(nodes.values(), True) / len(nodes)
         self.failure_handlers: list[FailureHandler] = [handler(name) for handler in handlers]
+
+    @classmethod
+    def get_nodes(cls, chainable: Chainable, required: bool = True) -> dict[Node, bool]:
+        """
+        extracts all the chain nodes and determine if they are required or optional
+        depending on their roots, they are considered required if neither
+        they nor their parents are optional.
+
+        :param chainable: Chainable subclass instance.
+        :type chainable: Node | Collection
+        :param required: holds previous required state when recusing
+        :type required: bool
+        :return: dict mapping nodes to their required state.
+        :rtype: dict[Node, bool]
+        """
+        required = required and not chainable.optional
+        nodes: dict[Node, bool] = dict()
+        if isinstance(chainable, Collection):
+            for member in chainable.members:
+                nodes.update(cls.get_nodes(member, required))
+        elif isinstance(chainable, Node):
+            nodes[chainable] = required
+        return nodes
 
     def __call__(self) -> Reporter:
         """creates a new Reporter object with the previously specified information."""
-        return Reporter(self.components, self.required, self.failure_handlers)
+        return Reporter(self.components, self.minimum_rate, self.failure_handlers)
---
+++ tests/unittests/test_monitoring.py
@@ -0,0 +1,169 @@
+import itertools
+import pytest
+from pytest import param
+
+from fastchain.chainables import *
+from fastchain.chainables.options import *
+from fastchain.factory import *
+from fastchain.monitoring import *
+
+
+class _FailureHandler_(FailureHandler):
+    name: str = "failure handler"
+
+    def __init__(self, owner: str) -> None:
+        super().__init__(owner)
+        self.called = False
+
+    def __call__(self, failure) -> None:
+        self.called = True
+
+
+FailureHandlers = LoggingHandler, _FailureHandler_  # to be extended easily
+HandlersCombination = tuple(itertools.chain(*(itertools.combinations(FailureHandlers, r) for r in range(len(FailureHandlers) + 1))))  # noqa
+
+
+def func(_): pass
+
+
+# test reporter maker
+
+SETUP_CASES = [
+    "(func,)",
+    "('?', func)",
+    "(func, func)",
+    "(func, '?', func)",
+    "('?', func, '?', func)",
+    "(func, '?', (func, func))",
+    "match(func, (func, '?', func))",
+    "[func, ('?', func)]",
+    "[func, ('?', (func, func))]",
+    "{'required': func, 'optional': ('?', func)}",
+]
+REQUIRED_NODES = [1, 0, 2, 1, 0, 1, 2, 1, 1, 1]
+TOTAL_NODES = [1, 1, 2, 2, 2, 3, 3, 2, 3, 2]
+MINIMUM_RATE = ['1', '0', '1', '1/2', '0', '1/3', '2/3', '1/2', '1/3', '1/2']
+
+
+@pytest.mark.parametrize('setup, required, total', zip(SETUP_CASES, REQUIRED_NODES, TOTAL_NODES), ids=repr)
+def test_get_nodes(setup, required, total):
+    res = ReporterMaker.get_nodes(parse(eval(setup)))  # type: ignore[parse]
+    assert all(isinstance(c, Node) for c in res)
+    assert operator.countOf(res.values(), True) == required
+    assert len(res) == total
+
+
+@pytest.mark.parametrize('handlers', HandlersCombination, ids=repr)
+@pytest.mark.parametrize('setup, minimum_rate', zip(SETUP_CASES, MINIMUM_RATE), ids=repr)
+def test_reporter_maker(setup, minimum_rate, handlers):
+    chainable_object = parse(eval(setup))
+    rm = ReporterMaker('test', chainable_object, handlers)
+    reporter = rm()
+    assert isinstance(reporter, Reporter)
+    for handler, handler_type in zip(rm.failure_handlers, handlers, strict=True):
+        assert isinstance(handler, handler_type)
+        assert handler.owner == 'test'
+    assert reporter.failure_handlers is rm.failure_handlers
+    assert rm.components == frozenset(ReporterMaker.get_nodes(chainable_object))
+    assert rm.minimum_rate == eval(minimum_rate)
+    assert reporter.minimum_rate == rm.minimum_rate
+
+
+@pytest.mark.parametrize('args_source, Error, msg', [
+    ("None, []", TypeError, "chainable must be an instance of Chainable subclass"),
+    ("object(), []", TypeError, "chainable must be an instance of Chainable subclass"),
+    ("parse((func, func)), [None]", TypeError, "handlers must be a list of fastchain.monitoring.FailureHandler subclasses"),  # noqa
+    ("parse((func, func)), [float]", TypeError, "handlers must be a list of fastchain.monitoring.FailureHandler subclasses"),  # noqa
+], ids=repr)
+def test_reporter_maker_validation(args_source, Error, msg):
+    with pytest.raises(Error, match=msg):
+        ReporterMaker('test', *eval(args_source))
+
+
+# test reporter
+@pytest.fixture
+def nodes(): return tuple(Node(func, name=f'node{i+1}') for i in range(4))
+@pytest.fixture
+def reporter(nodes): return Reporter(frozenset(nodes), 1, [])
+
+
+def test_reporter_creation(nodes):
+    reporter = Reporter(frozenset(nodes), 1, [])
+    assert reporter.counter == {node: [] for node in nodes}
+    assert reporter.minimum_rate == 1
+    assert reporter.failures == []
+    assert reporter.failure_handlers == []
+
+
+@pytest.mark.parametrize('combination, rate, successes, failures, missed', [
+    param([[True], [True], [True], [True]], 4/4, 4, 0, 0, id="single success marks"),
+    param([[True, True, True, True], [True], [True], [True, True]], 8/8, 8, 0, 0, id="multiple success marks"),
+    param([[False], [False], [False], [False]], 0/4, 0, 4, 0, id="single fail marks"),
+    param([[False, False], [False], [False], [False, False]], 0/6, 0, 6, 0, id="multiple fail marks"),
+    param([[True], [True], [False], [False]], 2/4, 2, 2, 0, id="single hybrid marks"),
+    param([[True, True, False], [True], [True, False], [False]], (2/3+1+1/2+0)/4, 4, 3, 0, id="multiple hybrid marks"),
+    param([[True], [True], [False], []], 2/4, 2, 1, 1, id="single last one missed"),
+    param([[True, True], [True, False], [], []], (1+1/2+0+0)/4, 3, 1, 2, id="multiple last two missed"),
+])
+def test_reporter_statistics(reporter, nodes, combination: list[list[bool]], rate: float, successes, failures, missed):
+    for node, states in zip(nodes, combination):
+        for state in states:
+            reporter(node, state)
+    statistics = reporter.statistics()
+    assert statistics['success_rate'] == round(rate, 4)
+    assert statistics['successful_node_operations'] == successes
+    assert statistics['failed_node_operations'] == failures
+    assert statistics['missed_nodes'] == missed
+
+
+def test_report_failure(nodes):
+    failure_handler = _FailureHandler_('test')
+    reporter = Reporter(frozenset(nodes), 1, [failure_handler])
+    error = ValueError('test')
+    assert not failure_handler.called
+    reporter.register_failure('test', None, error, fatal=True)
+    assert failure_handler.called
+    assert reporter.failures == [FailureDetails(source="test", input=None, error=error, fatal=True)]
+
+
+@pytest.mark.parametrize('reps', (0, 1, 2, 3), ids=repr)
+@pytest.mark.parametrize('source, input, error, fatal', itertools.product(('test',),
+                                                                          (None, ''),
+                                                                          (ValueError('test'), TypeError('test')),
+                                                                          (True, False)), ids=repr)
+def test_report_multiple_failures(reporter, source: str, input, error: Exception, fatal: bool, reps: int):
+    for _ in range(reps):
+        reporter.register_failure(source, input, error, fatal)
+    assert reporter.failures == [FailureDetails(source=source, input=input, error=error, fatal=fatal)] * reps
+
+
+@pytest.mark.parametrize('success_stat', (True, False))
+def test_marking_unregistered_node(reporter, success_stat):
+    node = Node(func)
+    with pytest.warns(UserWarning, match=f'unregistered node {node!r} ignored'):
+        reporter(node, success_stat)
+
+
+# test failure handlers common
+def test_failure_handler():
+    failure_handler = _FailureHandler_('test')
+    assert failure_handler.owner == 'test'
+    fd = FailureDetails(source='test_source', input='oops', error=ValueError('test'), fatal=True)
+    message = "test::test_source raised ValueError('test') after receiving 'oops' (type: str)"
+    assert failure_handler.message(fd) == message
+
+
+# test logging handler
+@pytest.mark.parametrize('level, fatal', [
+    (logging.INFO, False),
+    (logging.ERROR, True)
+], ids=['fatal=False/INFO', 'fatal=True/ERROR'])
+def test_logging_failure_handler(caplog, level, fatal):
+    logging_handler = LoggingHandler('test_logging_handler')
+    assert isinstance(logging_handler.logger, logging.Logger)
+    message = "test_logging_handler::test_source raised Exception('test') after receiving None (type: NoneType)"
+    with caplog.at_level(level, logging_handler.logger.name):
+        logging_handler(FailureDetails(source='test_source', input=None, error=Exception('test'), fatal=fatal))
+        log = caplog.records[-1]
+        assert log.levelname == logging.getLevelName(level)
+        assert log.message == message
